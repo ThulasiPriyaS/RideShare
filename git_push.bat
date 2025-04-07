f583787a212693ad7f7bd3d7a70275d7a23ce48c
ThulasiPriyaS <167528964+ThulasiPriyaS@users.noreply.github.com>
2025-04-07 16:09:00 +0530

    Initial commit

--- git_push.bat
+++ git_push.bat
@@ -2,5 +2,6 @@ git init
 git remote add origin https://github.com/ThulasiPriyaS/RideShare
 git add .
 git commit -m "Initial commit"
-git pull origin main --allow-unrelated-histories -m "Merge remote changes"
+git pull origin main --allow-unrelated-histories
+git merge origin/main -m "Merge remote changes"
 git push -u https://github.com/ThulasiPriyaS/RideShare main
